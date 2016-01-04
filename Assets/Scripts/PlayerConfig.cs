f19d7abc4f8ddab331aacd35a6cb60559eb6141a
Jure Debeljak <jure.debeljak@outfit7.com>
2016-01-04 18:50:21 +0100

    Halfway there

---
+++ Assets/Scripts/PlayerConfig.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using com.FDT.GameManagerFramework;
+using UnityEngine.UI;
+
+class PlayerConfig
+{
+	public PlayerConfig (int i, string text)
+	{
+		throw new System.NotImplementedException ();
+	}
+}
+
